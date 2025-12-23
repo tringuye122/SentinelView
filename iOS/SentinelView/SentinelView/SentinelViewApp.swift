eace11e22ca290b34ab10be8dea113d42a0420fc
Tri <tringuye@unc.edu>
2025-12-23 13:18:01 -0500

    Initial Swift Porject files

---
+++ iOS/SentinelView/SentinelView/SentinelViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SentinelViewApp.swift
+//  SentinelView
+//
+//  Created by Tri Nguyen on 12/23/25.
+//
+
+import SwiftUI
+
+@main
+struct SentinelViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
